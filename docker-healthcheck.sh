bb8146d9d11782accd070a950e37630d906ebb0d
Kenzi Stewart <kstewart@dployr.com>
2021-05-26 10:22:10 -0700

    Updated healthcheck to take DOCKER_HOSTNAME as an arg in buildconfig

--- docker-healthcheck.sh
+++ docker-healthcheck.sh
@@ -4,6 +4,6 @@ set -e
 
 # shellcheck disable=SC2001
 DOCKER_HEALTHCHECK_PATH=${DOCKER_HEALTHCHECK_PATH:-/healthcheck.html}
-SERVICE_NAME=$(cat "/etc/spear-repository" | sed -e "s%^.*/%%g")
+SERVICE_NAME=${DOCKER_HOSTNAME:-$(cat "/etc/spear-repository" | sed -e "s%^.*/%%g")}
 wget -O/dev/null -q --header "Host: ${SERVICE_NAME}${DOMAIN}" "http://localhost${DOCKER_HEALTHCHECK_PATH}" || exit 1
 exit 0
